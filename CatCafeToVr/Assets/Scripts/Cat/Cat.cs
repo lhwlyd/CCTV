a5cb96926f03926f5405d6c111f2a406c0f8e556
Roy Y. Luo <yil623@ucsd.edu>
2018-02-24 09:25:39 -0800

    first commit

--- CatCafeToVr/Assets/Scripts/Cat/Cat.cs
+++ CatCafeToVr/Assets/Scripts/Cat/Cat.cs
@@ -2,28 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cat : MonoBehaviour {
+
+// C:\acsnfs4.ucsd.edu\CifsHomes\648\q3dong\GitHub
+
+public class Cat : MonoBehaviour
+{
 
     private int moodPoints;
     Animation catAnimation;
-    AnimationClip[] clips;
 
-    public void getTouched() {
+    AnimationClip idle;
+    AnimationClip idle1;
+    AnimationClip idle2;
+    AnimationClip walk;
+    AnimationClip attack;
+    AnimationClip jump;
+
+    public Transform target;
+
+
+    public void getTouched()
+    {
         // Logic for mood
     }
 
-    public void increaseMood(int pts) {
+    public void increaseMood(int pts)
+    {
         moodPoints += pts;
     }
 
-    public void decreaseMood(int pts) {
+    public void decreaseMood(int pts)
+    {
         moodPoints -= pts;
     }
 
 
-    public Transform target;
+    public void walkTo(Transform target) {
+        this.target = target;
+
+        playClip(walk);
+    }
+
+
     public float smoothTime = 15F;
     private Vector3 velocity = Vector3.zero;
+
     void Update()
     {
         Vector3 targetPosition = target.transform.position;
@@ -34,10 +57,23 @@ public class Cat : MonoBehaviour {
     private void Awake()
     {
         catAnimation = this.gameObject.GetComponent<Animation>();
-        Debug.Log("Number of anims: " + catAnimation.GetClipCount());
-        AnimationClip clip = catAnimation.GetClip("Cat_arm|attack");
-        catAnimation.clip = clip;
+        assignClips();
+
+    }
+
+    private void playClip(AnimationClip inputClip) {
+        catAnimation.clip = inputClip;
         catAnimation.Play();
-        //List<AnimationState> states = new List<AnimationState>(catAnimation.Cast<AnimationState>());
+    }
+
+    private void assignClips()
+    {
+        idle = catAnimation.GetClip("Cat_arm|_idle_1");
+        idle1 = catAnimation.GetClip("Cat_arm|Idle_2");
+        idle2 = catAnimation.GetClip("Cat_arm|sit_idle");
+        walk = catAnimation.GetClip("Cat_arm|Walk");
+        attack = catAnimation.GetClip("Cat_arm|attack");
+        jump = catAnimation.GetClip("Cat_arm|jump");
+
     }
 }
